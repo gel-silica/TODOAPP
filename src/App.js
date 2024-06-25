2122f0c8d06699fbed779ed5ba53f4b59691901b
silicagel <ksk19981004@gmail.com>
2024-06-25 22:56:59 +0900

    インプットフォームのコンポーネントをApp.jsに追加

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { InputForm } from './components/InputForm';
 import { Title } from './components/Title';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="body">
       <Title />
+      <InputForm />
     </div>
   );
 }
